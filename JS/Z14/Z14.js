675162935e8fd903d7bf2f2de6d6ff10ff87247a
Nik <karpuk.nik@gmail.com>
2023-03-20 21:11:29 +0200

    push

--- JS/Z14/Z14.js
+++ JS/Z14/Z14.js
@@ -1,1 +1,14 @@
-use 
+"use strict"
+
+function factorial(m) {
+  let pow = 1;
+  for (let i = 1; i <= m; i++) {
+    pow *= i;    
+  }
+  return pow;
+}
+let n;
+while (isNaN(n) || !null) {
+  n = prompt ('Write number', n);
+}
+alert (factorial(n))
