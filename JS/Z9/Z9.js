4ba508615c57fbb24485a0adeb0b2ad2c4388da7
Nik <karpuk.nik@gmail.com>
2023-04-05 21:19:48 +0300

    changed

--- JS/Z9/Z9.js
+++ JS/Z9/Z9.js
@@ -1,23 +1,23 @@
 "use strict"
 
 let arr = ["Kharkiv", "Kiev", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper"];
-let point = document.querySelector(".firstDiv");
-function addElemetns(arr, point) {
-  let ul = document.createElement('ul');
-  ul.classList.add('list');
-  point.append(ul);
-  point = point.getElementsByClassName('list')[0];
-  arr.map(elem => {
-    if(Array.isArray(elem)) {
-      addElemetns(elem, point);
+
+function addElemetns(arr) {
+  let new1 = arr.map(elem => {
+    if(!Array.isArray(elem)){
+      return `<li>${elem}</li>`;
     } else {
-      let li = document.createElement('li');
-      li.innerText = elem;
-      li.classList.add('li');
-      point.append(li)
+      return `<ul>${addElemetns(elem)}</ul>`;
     };
-  });  
+  })
+  .join("");
+  console.log(new1);
+  return new1;
 };
+
+let ul = document.createElement('ul');
+ul.innerHTML = addElemetns(arr);
+document.body.append(ul);
 
 let i = 3;
 document.body.insertAdjacentHTML("afterend", `  <p class="counter"> List will be removed in 3 sec </p>`);
@@ -26,10 +26,10 @@ let timer = setInterval(() => {
   document.querySelector(".counter").remove();
   document.body.insertAdjacentHTML("afterend", `  <p class="counter"> List will be removed in ${i} sec </p>`);
   if(i === 0) {
-    document.querySelector('.list').remove();
+    document.querySelector('ul').remove();
     document.querySelector(".counter").remove();
     clearInterval(timer);
   };
 }, 1000);
 
-addElemetns(arr, point);
+
