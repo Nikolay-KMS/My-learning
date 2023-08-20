bf1502cbfe8d1e52e5b8e2826a44180c9f9ffc1f
NikKS <karpuk.nik@gmail.com>
2023-08-20 14:32:45 +0300

    push

---
+++ React/z5/src/router/pages/basketPage/BasketPage.jsx
@@ -0,0 +1,50 @@
+import { BasketCards } from './BasketCards';
+import { Modal } from '../../../Components/Modal';
+import { useDispatch, useSelector } from 'react-redux';
+import { BasketFooter } from './BasketFooter';
+
+export  function BasketPage() {
+  const cards = useSelector(state => state.cards);
+  const idCardsInBasket = useSelector(state => state.idCardsInBasket);
+  const isModalSecondActiveId = useSelector(state => state.isModalSecondActiveId);
+  const dispatch = useDispatch();
+
+  function handleClickDelete(deletedId) {
+    dispatch({type: 'REMOVE_CARD_IN_BASKET', payload: {id: deletedId}})
+  }
+
+  function handleClickModal(id) {
+    dispatch({type: 'PASS_ID_CARD_TO_IS_SECOND_MODAL_ACTIVE', payload: {id: id}})
+  }
+
+  return (
+    <div>
+      <div className='main column'>
+        <BasketCards 
+          cards={cards.filter(card => Object.keys(idCardsInBasket).includes(`${card.id}`))}
+          handleClickBtn={(id) => {
+            handleClickModal(id)
+          }}
+          isStar={false}
+          isButton={false}
+          isCloseButton={true}
+        />
+        {isModalSecondActiveId &&
+          < Modal
+            headerText={"Do you want to remove this item from your shopping cart?"}
+            bodyText={"Do you really want to do it?"}
+            handleCloseModal={() => {
+              dispatch({type: 'PASS_FALSE_TO_IS_SECOND_MODAL_ACTIVE'})
+            }}
+            isCloseButton={true}
+            id={isModalSecondActiveId}
+            handleClickBtn={(id) => handleClickDelete(id)}
+          />}
+      </div>
+      <BasketFooter 
+        cards={cards.filter(card => Object.keys(idCardsInBasket).includes(`${card.id}`))}
+      />
+    </div>
+
+  )
+}
